6ee22e4301af0ee70e6c266ea3c34fe542367c13
Sam McLeod <smj@fastmail.com>
2021-08-29 10:12:40 +1000

    aliasews

--- bootstrap_shell.sh
+++ bootstrap_shell.sh
@@ -8,8 +8,10 @@
 # - Internet access
 # - Homebrew, git installed
 
-brew install fzf tmux tmux-cssh zsh git sekey gpg jq youtube-dl nmap mtr testssl ncdu xz kube-ps1
-brew cask install appcleaner handbrake imageoptim launchcontrol onyx wireshark stay
+brew install fzf tmux tmux-cssh zsh git sekey gpg jq youtube-dl nmap mtr testssl \
+  ncdu xz kube-ps1 appcleaner handbrake imageoptim launchcontrol onyx stay wireshark \
+  speedtest-cli jq awscli iperf3
+  
 
 grep -q -F '/usr/local/bin/zsh' /etc/shells || echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 
@@ -25,6 +27,12 @@ fi
 if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then
   mkdir -p ~/.tmux/plugins
   git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+fi
+
+# Setup global gitignore file
+if [ ! -d "${HOME}/.gitignoreglobal" ]; then
+  ln -s $HOME/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dropbox\ Import/dotfiles/shell_config/.gitignoreglobal $HOME/.gitignoreglobal
+  git config --global core.excludesfile ~/.gitignoreglobal
 fi
 
 rm -f ~/.zshrc
